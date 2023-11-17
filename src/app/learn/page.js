44d5a480bf10481322b6fecfcd6383d1e1b21fac
wry420 <nishchalns10@gmail.com>
2023-11-17 16:45:46 +0545

    added drawer

--- src/app/learn/page.js
+++ src/app/learn/page.js
@@ -1,14 +1,26 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/ui/theme-toggle";
+import { Drawer } from "@/components/navbar/drawer";
+import { useState } from "react";
 
 export default function Home() {
+  const [isChecked, setIsChecked] = useState(false);
+  const handleCheck = (event) => {
+    setIsChecked(event.target.checked);
+    console.log("Checkbox is checked:", event.target.checked);
+  };
+
   return (
     <main className="">
-      <nav className="flex flex-row-reverse top-0 p-4 bg-muted">
+      <nav className="flex flex-row-reverse top-0 p-3 bg-muted">
         <ModeToggle className="flex-shrink" />
 
         <text className="mx-auto text-3xl font-semibold">Learn</text>
+        <Drawer handleCheck={handleCheck} />
       </nav>
+
+      <div>hi</div>
     </main>
   );
 }
